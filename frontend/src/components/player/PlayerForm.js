269b0bda05a3b3bc9a9e17495ab14fb5047f12f0
edriso <edriso.dev@gmail.com>
2023-02-15 20:21:34 +0200

    add PlayerForm basic structure

--- frontend/src/components/player/PlayerForm.js
+++ frontend/src/components/player/PlayerForm.js
@@ -1,7 +1,47 @@
 function PlayerForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("added");
+  };
+
   return (
-    <div>
-      <p>player form</p>
+    <div className="row">
+      <h3 className="center">Add a new player</h3>
+
+      <form className="col s12" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="first_name" name="firstName" type="text" />
+            <label htmlFor="first_name">First Name</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input id="last_name" name="lastName" type="text" />
+            <label htmlFor="last_name">Last Name</label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="phone" name="phone" type="text" />
+            <label htmlFor="phone">Phone</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input id="email" name="email" type="email" />
+            <label htmlFor="email">Email</label>
+          </div>
+        </div>
+
+        <button
+          className="btn waves-effect waves-light yellow darken-2"
+          type="submit"
+          name="action"
+        >
+          Add player
+        </button>
+      </form>
     </div>
   );
 }
